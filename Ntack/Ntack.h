4cdc872eb162b6a46d9a695332e77586fc6d1f65
Ilter Cengiz <iltercengiz@icloud.com>
2016-10-17 20:41:08 +0300

    Initial Commit

---
+++ Ntack/Ntack.h
@@ -0,0 +1,19 @@
+//
+//  Ntack.h
+//  Ntack
+//
+//  Created by Ilter Cengiz on 17/10/2016.
+//  Copyright © 2016 Ilter Cengiz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Ntack.
+FOUNDATION_EXPORT double NtackVersionNumber;
+
+//! Project version string for Ntack.
+FOUNDATION_EXPORT const unsigned char NtackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ntack/PublicHeader.h>
+
+
---
+++ NtackTests/NtackTests.m
@@ -0,0 +1,39 @@
+//
+//  NtackTests.m
+//  NtackTests
+//
+//  Created by Ilter Cengiz on 17/10/2016.
+//  Copyright © 2016 Ilter Cengiz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NtackTests : XCTestCase
+
+@end
+
+@implementation NtackTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
